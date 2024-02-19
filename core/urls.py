0ecc8bd48c04bf8f3b8d1560ed37c88e1e16a9c6
jeffnyalik <jeffnyak@gmail.com>
2024-02-19 11:46:01 +0300

    tests added

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('api.urls'), name='api')
+    path('api/v1/', include('api.urls'), name='api'),
+    path('snippets/v1', include('snipptes.urls'), name='snipptes')
 ]
